e8aeeaf5d7442c449fc32ce7d3b8ed354d826f29
liangxtian <liangxtian@icloud.com>
2019-11-05 16:03:51 +0800

    更新12

--- src/com/atguigu/rgit/test/HelloEgit.java
+++ src/com/atguigu/rgit/test/HelloEgit.java
@@ -3,8 +3,8 @@ package com.atguigu.rgit.test;
 public class HelloEgit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello Egit!");
+		System.out.println("更新12");
 	}
 
 }
